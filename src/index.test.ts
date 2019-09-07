ed2bcd82a578834fef1714d90c92690e8a99a64a
Joe Gallegos <joegallegos@Joes-MacBook-Pro.local>
2019-09-07 12:23:01 -0600

    Added addtional tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,8 +2,22 @@ import { Log } from './index'
 
 const mockLog = new Log()
 
-it('returns void', () => {
+it('debug returns void', () => {
   const result = mockLog.debug('hello world')
   expect(result).toEqual(undefined)
 })
 
+it('error with multiple args returns void', () => {
+  const result = mockLog.error('error', {error: 'testing error'})
+  expect(result).toEqual(undefined)
+})
+
+it('info with multiple args returns void', () => {
+  const result = mockLog.info('info', {info: 'testing info'})
+  expect(result).toEqual(undefined)
+})
+
+it('warning with multiple args returns void', () => {
+  const result = mockLog.warn('warn', {warning: 'testing warning'})
+  expect(result).toEqual(undefined)
+})
